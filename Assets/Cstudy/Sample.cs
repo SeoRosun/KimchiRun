a047415c6c955cb3913a06a4a4ebeeb52e6f8494
SeoRosun <naruharukamiki@gmail.com>
2026-03-27 15:16:26 +0900

    .

--- Assets/Cstudy/Sample.cs
+++ Assets/Cstudy/Sample.cs
@@ -5,7 +5,8 @@ public class Sample : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int i = 1;
+        Debug.Log($"{i}번 적 등장!");
     }
 
     // Update is called once per frame
